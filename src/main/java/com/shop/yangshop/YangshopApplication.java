b1b23c8d3e4203b87e0708a5cb666ad014748bd4
linxj3 <1255731632@qq.com>
2021-03-20 16:34:07 +0800

    Initial commit

---
+++ src/main/java/com/shop/yangshop/YangshopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.yangshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/yangshop/YangshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.yangshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
